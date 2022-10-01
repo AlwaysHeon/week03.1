7c1955efde2366cd7b1f72b3bc16f514505a8bf8
always_heon <cfohan1@gail.com>
2022-10-01 22:54:23 +0900

    1차 완료

---
+++ src/Transportation.java
@@ -0,0 +1,12 @@
+public class Transportation {
+    int num;
+    int fuel = 100;
+    int refuel = 0;
+    int currentGas = fuel - refuel;
+    boolean gasLeft = currentGas != 0;
+    int speed = 0;
+    int speedChange;
+    int maxPass;
+
+
+}
